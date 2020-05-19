bfb66698ddc2dd96bfdf4c58a2dcaeb442225a2d
khmilevoi <sergei@devico.io>
2020-05-19 15:50:57 +0300

    finish

--- lb1/index.js
+++ lb1/index.js
@@ -49,22 +49,23 @@ const parseInformation = (html) => {
     window: { document },
   } = html;
 
-  const items = document.getElementsByClassName("product-thumb");
+  const items = document.getElementsByClassName("obj-box");
 
   return Array.from(Array(items.length), (_, index) => {
     const item = items.item(index);
 
-    const product = {};
+    const [name] = item.getElementsByClassName("product-name");
+    const [brand] = item.getElementsByClassName("brand-caption");
+    const [description] = item.getElementsByClassName("obj-box-desc");
+    const [price] = item.getElementsByClassName("price");
 
-    const metaInfo = item.querySelectorAll("meta");
-
-    for (let meta of metaInfo) {
-      const [name, value] = parseMeta(meta);
-
-      product[name] = value;
-    }
-
-    return product;
+    return {
+      id: index + 5000,
+      name: name.innerHTML,
+      brand: brand.innerHTML,
+      description: description.innerHTML,
+      price: price.innerHTML,
+    };
   });
 };
 
@@ -123,7 +124,7 @@ const wrapXML = (xml) => {
 
 const main = async () => {
   try {
-    const data = await readURL("https://smt.ua/asic_miner/");
+    const data = await readURL("https://tt-maximum.com/shop/balls");
     const html = data.toString();
 
     const parsedHTML = new JSDOM(html);
