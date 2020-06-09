b1ad06405257ba12de68082d8d00cf22b3e7430e
devicohub <66035070+devicohub@users.noreply.github.com>
2020-06-09 12:34:02 +0300

    Update readFile.ts

--- src/utils/readFile.ts
+++ src/utils/readFile.ts
@@ -1,8 +1,9 @@
 import * as fs from "fs";
+import * as path from "path";
 
-export const readFile = async (path: string) =>
+export const readFile = async (pathTo: string) =>
   new Promise<Buffer>((resolve, reject) => {
-    fs.readFile(path, (err, data) => {
+    fs.readFile(path.join(__dirname, pathTo), (err, data) => {
       if (err) {
         reject(err);
       }
